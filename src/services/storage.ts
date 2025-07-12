3d626d11613f673b32dd8de4c1691d5ff952bbaf
Matt Farmer <matt@frmr.me>
2025-07-12 14:36:15 -0400

    feat: upgrade to awssdkv3

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,32 +1,40 @@
-import AWS from 'aws-sdk';
+import { 
+  S3Client, 
+  HeadBucketCommand, 
+  CreateBucketCommand, 
+  PutObjectCommand,
+  GetObjectCommand
+} from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 import { Config } from '../config';
 
 export class StorageService {
-  private s3: AWS.S3;
+  private s3: S3Client;
 
   constructor() {
-    const s3Config: AWS.S3.ClientConfiguration = {
-      accessKeyId: Config.s3.accessKeyId,
-      secretAccessKey: Config.s3.secretAccessKey,
+    const s3Config = {
+      credentials: {
+        accessKeyId: Config.s3.accessKeyId,
+        secretAccessKey: Config.s3.secretAccessKey,
+      },
       region: Config.s3.region,
-      signatureVersion: 'v4',
     };
 
-    // Only set endpoint and s3ForcePathStyle for non-AWS S3 services (like MinIO)
+    // Only set endpoint and forcePathStyle for non-AWS S3 services (like MinIO)
     if (Config.s3.endpoint) {
-      s3Config.endpoint = Config.s3.endpoint;
-      s3Config.s3ForcePathStyle = true;
+      (s3Config as any).endpoint = Config.s3.endpoint;
+      (s3Config as any).forcePathStyle = true;
     }
 
-    this.s3 = new AWS.S3(s3Config);
+    this.s3 = new S3Client(s3Config);
   }
 
   async ensureBucketExists(): Promise<void> {
     try {
-      await this.s3.headBucket({ Bucket: Config.s3.bucket }).promise();
+      await this.s3.send(new HeadBucketCommand({ Bucket: Config.s3.bucket }));
     } catch (error: any) {
-      if (error.statusCode === 404) {
-        await this.s3.createBucket({ Bucket: Config.s3.bucket }).promise();
+      if (error.$metadata?.httpStatusCode === 404) {
+        await this.s3.send(new CreateBucketCommand({ Bucket: Config.s3.bucket }));
       } else {
         throw error;
       }
@@ -34,25 +42,23 @@ export class StorageService {
   }
 
   async uploadImage(buffer: Buffer, fileName: string): Promise<void> {
-    const params = {
+    const command = new PutObjectCommand({
       Bucket: Config.s3.bucket,
       Key: fileName,
       Body: buffer,
       ContentType: 'image/png',
-      ACL: 'private',
-    };
+    });
 
-    await this.s3.upload(params).promise();
+    await this.s3.send(command);
   }
 
   async generatePresignedUrl(fileName: string): Promise<string> {
-    const params = {
+    const command = new GetObjectCommand({
       Bucket: Config.s3.bucket,
       Key: fileName,
-      Expires: Config.presignedUrlExpiry,
-    };
+    });
 
-    return this.s3.getSignedUrl('getObject', params);
+    return await getSignedUrl(this.s3, command, { expiresIn: Config.presignedUrlExpiry });
   }
 
   generateFileName(): string {
